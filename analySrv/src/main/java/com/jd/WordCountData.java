047af69e494c1166608e6855747a5b502b5b70b3
gaowenkai <15629099560@163.com>
2019-09-18 22:22:55 +0800

    update

--- analySrv/src/main/java/com/jd/WordCountData.java
+++
@@ -1,9 +0,0 @@
-package com.jd;
-
-public class WordCountData {
-    public static final String[] WORDS = new String[] {
-            "To be, or not to be,--that is the question:--",
-            "Whether 'tis nobler in the mind to suffer",
-            "Be all my sins remember'd."
-    };
-}
