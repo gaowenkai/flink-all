177686bd88b03b6615095aaad45e1c52e35e587d
gaowenkai5 <gaowenkai5@jd.com>
2020-01-13 19:14:58 +0800

    leetcode

---
+++ analySrv/src/main/java/com/jd/study/leetcode/Soulation147.java
@@ -0,0 +1,32 @@
+package com.jd.study.leetcode;
+
+public class Soulation147 {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        ListNode next;
+
+        while(cur!=null){
+            next = cur.next;
+            if(next!=null&&cur.val>next.val){
+                while(pre.next!=null&&pre.next.val<next.val){
+                    pre = pre.next;
+                }
+                ListNode  t = pre.next;
+                pre.next = next;
+                cur.next = next.next;
+                next.next = t;
+
+                pre = dummy;
+            }else{
+                cur =next;
+            }
+        }
+
+
+
+        return dummy.next;
+    }
+}
