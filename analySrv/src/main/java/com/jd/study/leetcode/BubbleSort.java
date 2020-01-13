4805856ea025112e80d9dd03b9d8412f9f0e469b
gaowenkai5 <gaowenkai5@jd.com>
2020-01-13 17:09:53 +0800

    bubblesort

---
+++ analySrv/src/main/java/com/jd/study/leetcode/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.jd.study.leetcode;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] nums){
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,4,2,6,3,2,7,2,9};
+        bubbleSort(arr);
+        for (int i=0;i<arr.length-1;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
